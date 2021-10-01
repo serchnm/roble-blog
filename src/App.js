e1fc948fbdcf331c0b3f08b69cc99ea35efb3880
Serchnm <sergio.ortiz@tectijuana.edu.mx>
2021-09-30 22:27:40 -0700

    align

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Nav from './components/header'
+import Nav from './components/header';
+import HomePage from './views/home';
 
 function App() {
   return (
     <div>
       <Nav />
+      <HomePage />
     </div>
   );
 }
--- src/views/home.js
+++ src/views/home.js
@@ -1,1 +1,11 @@
 import React from "react";
+
+function HomePage() {
+    return (
+      <div className="homePage">
+        <p className="text-center">No hay otro Nombre</p>
+      </div>
+    );
+  }
+
+export default HomePage;
