bf3ac90d895dcd6fca2cb82a0466ea1a91ee562c
Serchnm <sergio.ortiz@tectijuana.edu.mx>
2021-11-02 13:29:40 -0700

    adding card with text

--- src/views/home.js
+++ src/views/home.js
@@ -8,10 +8,15 @@ function HomePage() {
         <div className="homePage">
           <p>No hay otro Nombre</p>
         </div>
+        
         <div className="homePage-body">
           <img width="600" src={imgAboutUs} alt="AboutUsImage"/>
-
         </div>
+        <div className="about-us-card">
+            <h4>Sobre Nosotros</h4>
+            <p>Roble es un proyecto que surgio de una proposito hacia mi vida, en el mundo se que hay gente que necesita un aliento de vida a su alma y que mejor que conectar a la gente para dejar saber que nadie esta solo, siempre estaras acompañado.</p>
+        </div>
+        
       </div>
     );
   }
