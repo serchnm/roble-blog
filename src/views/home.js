c4bded9b462c94ff63eac7aed0292b8d13c650f9
Serchnm <sergio.ortiz@tectijuana.edu.mx>
2021-11-02 12:46:00 -0700

    adding scss

--- src/views/home.js
+++ src/views/home.js
@@ -1,9 +1,17 @@
 import React from "react";
+import imgAboutUs from '../assets/images/aboutusimage.jpg';
+import '../assets/scss/homePage.scss';
 
 function HomePage() {
     return (
-      <div className="homePage">
-        <p>No hay otro Nombre</p>
+      <div>
+        <div className="homePage">
+          <p>No hay otro Nombre</p>
+        </div>
+        <div className="homePage-body">
+          <img width="600" src={imgAboutUs} alt="AboutUsImage"/>
+
+        </div>
       </div>
     );
   }
