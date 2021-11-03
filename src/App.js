91237b0ab20375c95c3880aa3cb383981e111d72
Serchnm <sergio.ortiz@tectijuana.edu.mx>
2021-11-02 18:08:52 -0700

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/header';
 import HomePage from './views/home';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div>
       <Nav />
       <HomePage />
+      <Footer />
     </div>
   );
 }
